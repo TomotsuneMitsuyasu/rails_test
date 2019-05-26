9b0e85111e7748dd0132327f40ff4c12ee983d2c
Tomo Mitsu <beckn2149@gmail.com>
2019-05-26 06:23:53 +0000

    　”削除ボタン見直し”

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,14 @@
 class TweetsController < ApplicationController
   before_action:set_tweet,only:[:show,:edit,:update,:destroy]
-    
-    
+
      def index
        @tweets = Tweet.all
      end
-     
+
      def new
        @tweet = Tweet.new
      end
-     
+
      def create
        @tweet = Tweet.new(tweet_params)
        if @tweet.save
@@ -17,17 +16,16 @@ class TweetsController < ApplicationController
       else
         render'new'
        end
-      
      end
-     
+
      def show
        @tweet = Tweet.find(params[:id])
      end
-     
+
      def edit
        @tweet = Tweet.find(params[:id])
      end
-     
+
      def update
        @tweet = Tweet.find(params[:id])
        if @tweet.update(tweet_params)
@@ -36,23 +34,22 @@ class TweetsController < ApplicationController
            render 'edit'
        end
      end
-     
+
      def destroy
        @tweet.destroy
        redirect_to tweets_path,notice:"Tweetを削除しました。"
      end
-     
+
      def confirm 
        @tweet = Tweet.new(tweet_params)
      end
-     
+
      private
-     
+
      def tweet_params
        params.require(:tweet).permit(:title,:content)
      end
-  
-  
+
      def set_tweet
        @tweet = Tweet.find(params[:id])
      end
